496ff20a352c331dc0087253c618a06a4259e7c2
Erdem AVLAGI <erdemavlagi@gmail.com>
2023-04-26 16:23:02 +0300

    feat: create contact page

--- resume/urls.py
+++ resume/urls.py
@@ -16,10 +16,9 @@ Including another URLconf
 """
 
 from django.contrib import admin
-from django.urls import path
-from core.views import index
+from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('',index, name='index'),
+    path('',include('core.urls'))
 ]
